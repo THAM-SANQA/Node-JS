860e340152833b22c91cef7cfdda7bb685a536e1
THAM-SANQA <ndabat020@gmail.com>
2023-08-19 13:46:02 +0200

    first commit

--- unit1/lesson6/server_html-1/main.js
+++ unit1/lesson6/server_html-1/main.js
@@ -2,22 +2,43 @@ const port = 3000,
     http = require("http"),
     httpStatus = require("http-status-codes"),
     fs = require("fs");
-const routeMap = {
-    "/": "views/index.html"
+// const routeMap = {
+//     "/": "views/index.html"
+// };
+// http
+//     .createServer((req, res) => {
+//         res.writeHead(httpStatus.OK, {
+//             "Content-Type": "text/html"
+//         });
+//         if (routeMap[req.url]) {
+//             fs.readFile(routeMap[req.url], (error, data) => {
+//                 res.write(data);
+//                 res.end();
+//             });
+//         } else {
+//             res.end("<h1>Sorry, not found.</h1>");
+//         }
+//     })
+//     .listen(port);
+// console.log(`The server has started and is listening on port number: ${port}`);
+const getViewUrl = url => {
+    return `views${url}.html`;
 };
 http
     .createServer((req, res) => {
-        res.writeHead(httpStatus.OK, {
-            "Content-Type": "text/html"
-        });
-        if (routeMap[req.url]) {
-            fs.readFile(routeMap[req.url], (error, data) => {
+        let viewUrl = getViewUrl(req.url);
+        fs.readFile(viewUrl, (error, data) => {
+            if (error) {
+                res.writeHead(httpStatus.NOT_FOUND);
+                res.write("<h1>FILE NOT FOUND</h1>");
+            } else {
+                res.writeHead(httpStatus.OK, {
+                    "Content-Type": "text/html"
+                });
                 res.write(data);
-                res.end();
-            });
-        } else {
-            res.end("<h1>Sorry, not found.</h1>");
-        }
+            }
+            res.end();
+        });
     })
     .listen(port);
 console.log(`The server has started and is listening on port number: ${port}`);
