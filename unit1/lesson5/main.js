e39e3e7b284990ebc2c7f56e640045dfc163c257
THAM-SANQA <ndabat020@gmail.com>
2023-08-31 08:09:56 +0200

    first commit

--- unit1/lesson5/main.js
+++ unit1/lesson5/main.js
@@ -13,12 +13,12 @@
 // app.listen(port);
 // console.log(`The server has started and is listening on port number: ${port}`);
 
-const routeResponseMap = {
+const routeResponseMap = { //defining maooing routes with responses
     "/info": "<h1>Info Page</h1>",
     "/contact": "<h1>Contact Us</h1>",
     "/about": "<h1>Learn More About Us.</h1>",
     "/hello": "<h1>Say hello by emailing us here</h1>",
-    "/error": "<h1>Sorry the page you are looking for is not here.</h1>"
+    "/error": "<h1>404. Sorry the page you are looking for is not here.</h1>"
 };
 const port = 3000,
     http = require("http"),
@@ -27,10 +27,10 @@ const port = 3000,
         res.writeHead(200, {
             "Content-Type": "text/html"
         });
-        if (routeResponseMap[req.url]) {
-            res.end(routeResponseMap[req.url]);
+        if (routeResponseMap[req.url]) { //check whether a request route is deined in the map
+            res.end(routeResponseMap[req.url]); //respond with default HTML 
         } else {
-            res.end("<h1>Welcome!</h1>");
+            res.end("<h1>Welcome!</h1>"); //respond with HTML to every request
         }
     });
 app.listen(port);
