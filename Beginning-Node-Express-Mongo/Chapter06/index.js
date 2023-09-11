05ea355cce02690efd22a3b7c69610c1875db430
THAM-SANQA <ndabat020@gmail.com>
2023-09-11 09:42:48 +0200

    first commit

---
+++ Beginning-Node-Express-Mongo/Chapter06/index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const BlogPost = require('./models/BlogPost.js');
+const app = new express();
+app.use(express.static('public'));
+// the next 2 functions enable the app to handle POST requests
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// With app.set('view engine','ejs'), we tell Express to use EJS as our templating engine, 
+// that any file ending in .ejs should be rendered with the EJS package.
+app.set('view engine', 'ejs');
+mongoose.connect('mongodb://127.0.0.1/my_database', {useNewUrlParser: true});
+
+app.get('/', async (req, res) => {
+    const blogposts = await BlogPost.find({});
+    res.render('index', {
+        blogposts
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.get('/post/:id', async (req, res) => {
+    const blogpost = await BlogPost.findById(req.params.id)
+    res.render('post', { blogpost });
+});
+
+app.get('/posts/new', (req, res) => {
+    res.render('create');
+});
+
+app.post('/posts/store', async (req, res) => {
+    await BlogPost.create(req.body)
+        .then(blogspot => { 
+            res.redirect('/')})
+        .catch(error => { 
+            console.log(error)})
+});
+
+app.listen(3000, () => {
+    console.log("App listening on port 3000");
+});
